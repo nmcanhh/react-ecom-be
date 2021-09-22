d3bf7cb766d834acd4745b3ed048312e6b19dd20
nmcanhh <nmcanhh@gmail.com>
2021-09-22 18:00:38 +0700

    Tạo Login với API

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -30,13 +30,41 @@ class AuthController extends Controller
             ]);
             $token = $user->createToken($user->email . '_Token')->plainTextToken;
             return response()->json([
-                'status' => '200',
+                'status' => 200,
                 'username' => $user->name,
                 'token' => $token,
                 'message' => 'Registered Successfully'
             ]);
         }
+    }
 
-
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email|max:191',
+            'password' => 'required',
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'validator_erros' => $validator->messages(),
+            ]);
+        } else {
+           $user = User::where('email', $request->email)->first();
+           if(!$user || ! Hash::check($request->password, $user->password))
+           {
+               return response()->json([
+                   'status' => 401,
+                   'message'=>'Invalid Credentials'
+               ]);
+           } else {
+               $token = $user->createToken($user->email . '_Token')->plainTextToken;
+               return response()->json([
+                   'status' => 200,
+                   'username' => $user->name,
+                   'token' => $token,
+                   'message' => 'Logged In Successfully'
+               ]);
+           }
+        }
     }
 }
